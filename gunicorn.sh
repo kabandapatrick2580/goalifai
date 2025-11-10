44902cb105c7c12bf006760c1f40f7dcffea4c91
kabandapatrick2580 <sendsome2019@gmail.com>
2025-11-10 08:36:32 +0200

    running in the background

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-gunicorn -k gevent --worker-connections 1000 -w 2 -b 0.0.0.0:8000 app:app
+nohup gunicorn -k gevent --worker-connections 1000 -w 2 -b 0.0.0.0:8000 app:app > gunicorn.log 2>&1 &
+echo "Gunicorn started in background (check gunicorn.log for logs)"
